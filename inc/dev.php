62e99fa53a352966c7379fca6b4a50d262511c1b
battmates <35600317+battmates@users.noreply.github.com>
2025-02-11 15:05:11 +0000

    Update dev.php

--- inc/dev.php
+++ inc/dev.php
@@ -17,3 +17,18 @@ function disable_admin_bar_for_non_admins() {
     }
 }
 add_action('after_setup_theme', 'disable_admin_bar_for_non_admins');
+
+// ACF options pages
+if( function_exists('acf_add_options_page') ) {
+    
+    acf_add_options_page(array(
+        'page_title'    => 'Rock School Theme Settings',
+        'menu_title'    => 'Theme Settings',
+        'menu_slug'     => 'theme-general-settings',
+        'capability'    => 'edit_posts',
+        'redirect'      => false,
+        'icon_url'      => 'dashicons-marker',
+        'position'      => 1
+    ));
+           
+}
