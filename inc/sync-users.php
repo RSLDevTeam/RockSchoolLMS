40e375f5b33a648e057a1c5a945530334661335c
Sarah Sajjad <sarahsajjad93@gmail.com>
2025-04-01 10:01:58 +0100

    added address and phone to cognito syncs

--- inc/sync-users.php
+++ inc/sync-users.php
@@ -12,7 +12,7 @@ define('SDK_ACCESS_KEY', get_field('access_key', 'option')?: '');
 define('SDK_SECRET_KEY', get_field('secret_key', 'option')?: '');
  
 add_action('user_register', 'sync_user_to_cognito', 10, 1);
-add_action('profile_update', 'sync_user_to_cognito', 10, 1);
+add_action('profile_update', 'sync_user_to_cognito', 10, 2);
 
 function sync_user_to_cognito($user_id) {
     $user = get_userdata($user_id);
@@ -21,7 +21,6 @@ function sync_user_to_cognito($user_id) {
         return;
     }
     
- 
     $client_id = SDK_ACCESS_KEY;
     $client_secret = SDK_SECRET_KEY;
     
@@ -36,11 +35,16 @@ function sync_user_to_cognito($user_id) {
  
     $userPoolId = 'eu-west-2_MjoR6V3RL';
     $username   = $user->user_login;
+    $userPhone  = get_user_meta($user_id, 'billing_phone', true) ?? "";
+    $userAddress = get_user_address($user_id);
  
     $attributes = [
         ['Name' => 'name', 'Value' => $user->display_name],
         ['Name' => 'email', 'Value' => $user->user_email],
         ['Name' => 'email_verified', 'Value' => 'true'],
+        ['Name' => 'phone_number', 'Value' => $userPhone],
+        ['Name' => 'phone_number_verified', 'Value' => 'true'],
+        ['Name' => 'address', 'Value' => $userAddress],
         ['Name' => 'custom:user_role', 'Value' => $user->roles[0] ?? 'subscriber'],
     ];
  
@@ -69,4 +73,34 @@ function sync_user_to_cognito($user_id) {
             wp_die('Error syncing user to Cognito' . $e->getMessage());
         }
     }
+}
+
+// function get address from user meta and jason signifying the address
+function get_user_address($user_id) {
+    $user = get_userdata($user_id);
+
+    // Get WooCommerce billing details
+    $billing_address_1  = get_user_meta($user_id, 'billing_address_1', true) ?? "";
+    $billing_address_2  = get_user_meta($user_id, 'billing_address_2', true) ?? "";
+    $billing_city       = get_user_meta($user_id, 'billing_city', true) ?? "";
+    $billing_postcode   = get_user_meta($user_id, 'billing_postcode', true) ?? "";
+    $billing_state      = get_user_meta($user_id, 'billing_state', true) ?? "";
+    $billing_country    = get_user_meta($user_id, 'billing_country', true) ?? "";
+
+
+    // Format address into a single string
+    $billing_address = trim("$billing_address_1 $billing_address_2, $billing_city, $billing_postcode, $billing_country");
+
+
+    $address = [
+          "formatted"        => $billing_address,
+          "street_address"   => $billing_address_1,
+          "extended_address" => $billing_address_2,
+          "locality"         => $billing_city,
+          "region"           => $billing_state,
+          "postal_code"      => $billing_postcode,
+          "country"          => $billing_country
+    ];
+
+    return json_encode($address);
 }
